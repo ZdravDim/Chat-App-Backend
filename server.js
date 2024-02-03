21d5eaee99e853c11eae5cca312a003426469299
unknown <dimitrijezdravkovic20@gmail.com>
2024-02-03 12:20:59 +0100

    add firestore

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const WebSocket = require('ws');
-const { v4: uuidv4 } = require('uuid');
-const { json } = require('express');
-const wss = new WebSocket.Server({ port: 3001 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA11_Orf7gStDculreYJlSbrD4ZVPhY9bQ",
+  authDomain: "chatapp-4d642.firebaseapp.com",
+  projectId: "chatapp-4d642",
+  storageBucket: "chatapp-4d642.appspot.com",
+  messagingSenderId: "711280795297",
+  appId: "1:711280795297:web:e1e9099de3d24df9bc1ae0",
+  measurementId: "G-H6FXF5V2JN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+import { WebSocketServer as Server } from 'ws';
+import { v4 as uuidv4 } from 'uuid';
+const wss = new Server({ port: 3001 });
 
 const clients = new Map();
 
@@ -19,7 +38,7 @@ wss.on('connection', (ws) => {
 
     clients.set(ws, metadata);
 
-    message = {
+    const message = {
         type: 1,
         id: id
     };
@@ -40,9 +59,9 @@ wss.on('connection', (ws) => {
     });
 
     ws.on("close", () => {
-        leftId = clients.get(ws)["id"];
+        const leftId = clients.get(ws)["id"];
         
-        message = {
+        const message = {
             type: 2,
             id: leftId
         };
@@ -53,19 +72,31 @@ wss.on('connection', (ws) => {
       });
 })
 
-//sends a message to everyone subscribed to the channel
+// sends a message to everyone subscribed to the channel
 function sendToAll(ws, message) {
-    senderId = clients.get(ws)["id"];
+    const senderId = clients.get(ws)["id"];
     message["time"] = (new Date()).getTime();
     
-    outbound = JSON.stringify(message);
+    const outbound = JSON.stringify(message);
 
     [...clients.keys()].forEach((client) => {
         if (clients.get(client)["id"] != senderId) client.send(outbound);
         else if (message["type"] == 0) { // only if it's standard message add attribute "sameUser", and set it to true
-            toSender = message
+            let toSender = message
             toSender["sameUser"] = true;
             client.send(JSON.stringify(toSender));
         }
     });
+
+    addMessageToFirestore(message);
 }
+
+// Function to add a message to Firestore
+async function addMessageToFirestore(message) {
+    try {
+      const docRef = await addDoc(collection(db, "messages"), message);
+      console.log("Message written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding message to Firestore: ", e);
+    }
+  }
