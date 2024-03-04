61c7026f623d2be515ff3f534522c72629f1eef8
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-03-04 12:32:41 +0100

    cors origin

--- httpServer.js
+++ httpServer.js
@@ -13,7 +13,7 @@ const app = express()
 
 const server = http.createServer(app)
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: "https://zdravdim.github.io/", credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
